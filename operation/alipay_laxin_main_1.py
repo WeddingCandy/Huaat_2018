7e01dcad15d79ea49eb0545b65fd3e55fc6f41b4
WeddingCandy <chenyouwenxyz@hotmail.com>
2018-08-02 22:04:50 +0800

    second commit

--- operation/alipay_laxin_main_1.py
+++ operation/alipay_laxin_main_1.py
@@ -7,4 +7,6 @@
 import pandas as pd
 match_info =  '/Users/Apple/Desktop/working/8 华院项目/运营自动化程序/match_info.xlsx'
 pd_match_info = pd.read_excel(match_info,encoding = 'utf-8',header=0)
-pd_match_info = pd_match_info.iloc[:,1:]
+columns_match_info = ['销售代表编码', '销售代表名称', '营业员手机号', '支付宝账号认证人', '营业员绑定支付宝', 'UID']
+pd_match_info_all = pd_match_info[columns_match_info]
+
